2b7cfd0bda1d847a88aee693c94bdf5f509085af
maxchisto <max.chisto@gmail.com>
2016-04-21 14:39:56 -0700

    Added Aplicative and Monad instances for Log

--- monad_demos/simpleLogger.hs
+++ monad_demos/simpleLogger.hs
@@ -4,6 +4,17 @@ data Log a = Log [String] a
 instance Functor Log where
     fmap f (Log xs a) = Log xs (f a)
 
+instance Applicative Log where
+    pure = returnLog
+    (Log xs f) <*> (Log ys val) = Log (xs ++ ys) (f val)
+    
+instance Monad Log where
+    return = returnLog
+    (>>=) = bindLog
+
+returnLog :: a -> Log a
+returnLog = \x -> Log [] x
+    
 toLogger :: (a -> b) -> String -> (a -> Log b)
 toLogger f msg = \x -> Log [msg] (f x)
 
@@ -12,21 +23,23 @@ execLoggers x f1 f2 = logC
     where (Log xs val1) = f1 x
           (Log ys val2) = f2 val1
           logC = Log (xs ++ ys) val2 
-          
 
+          
 bindLog :: Log a -> (a -> Log b) -> Log b
 bindLog (Log xs val) f = result
     where (Log ys val2) = f val
           result = Log (xs ++ ys) val2
+
+execLoggersList :: a -> [a -> Log a] -> Log a
+execLoggersList init xs = foldl (>>=) (returnLog init) xs 
+
+add1Log = toLogger (+1) "added one"
+mult2Log = toLogger (* 2) "multiplied by 2"
         
 -- Usage:
-    -- GHCi> let add1Log = toLogger (+1) "added one"
     -- GHCi> add1Log 3
     -- Log ["added one"] 4
-
-    -- GHCi> let mult2Log = toLogger (* 2) "multiplied by 2"
     -- GHCi> mult2Log 3
     -- Log ["multiplied by 2"] 6
-    
     -- GHCi> execLoggers 3 add1Log mult2Log
     -- Log ["added one","multiplied by 2"] 8
