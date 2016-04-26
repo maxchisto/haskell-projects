70e339dd087045d7c8256680171daf9b680d5290
maxchisto <max.chisto@gmail.com>
2016-04-25 17:44:28 -0700

    Replaced binary with `hs` file

---
+++ monad_demos/simpleGreeter.hs
@@ -0,0 +1,17 @@
+-- Asks for your name untill you enter it
+-- Then greets you.
+
+getName :: IO String
+getName = do name <- getLine
+             if (length name) < 1
+             then do putStrLn "What is your name?"
+                     getName
+             else return name
+
+main :: IO ()
+main = do putStrLn "What is your name?"
+          name <- getName
+          putStrLn $ "Hi, " ++ name ++ "!"
+
+        
+        
