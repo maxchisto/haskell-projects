fcbef2f005423fa3f7c42ead928aff3db036e050
maxchisto <max.chisto@gmail.com>
2016-04-28 16:45:11 -0700

    Aded a function to show the list of items purchased

--- monadDemos/shoppingCart.hs
+++ monadDemos/shoppingCart.hs
@@ -4,14 +4,17 @@
 import Control.Monad
 import Control.Monad.Writer
 
-type ShoppingCart = Writer (Sum Integer) ()
+type ShoppingCart = Writer ( [(String, Integer)] ) ()
 
 purchase :: String -> Integer -> ShoppingCart
-purchase name price = do tell (Sum price)
+purchase name price = do tell ([(name, price)])
                          return ()
 
 total :: ShoppingCart -> Integer
-total = getSum . execWriter
+total = (sum . map snd . execWriter)
+
+items :: ShoppingCart -> [String]
+items = (map fst . execWriter)
     
 listOfPurchases :: ShoppingCart
 listOfPurchases = do
