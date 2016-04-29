29fba8473b179f336b17f93b271e298de0b2ede5
maxchisto <max.chisto@gmail.com>
2016-04-29 16:55:10 -0700

    More comments

--- monadDemos/fibonacciStateMonad.hs
+++ monadDemos/fibonacciStateMonad.hs
@@ -2,11 +2,15 @@
 
 import Control.Monad.State
 
+-- Tuple represents two last number in the series.
+-- When executed, both numbers get bumped up to the next one it the series.
+-- For example (3,5) becomes (5,8)
 fibStep :: State (Integer,Integer) ()
 fibStep = do (a,b) <- get
              put (b, a+b)
              return ()
-             
+
+-- Helper function which executes a monad n times             
 execStateN :: Integer -> State s a -> s -> s
 execStateN n stateMonad init = execState (replicateM (fromIntegral n) stateMonad) init
 
