58fb56971aca44ab2dd84ce5833cb49c2f2dd1b9
maxchisto <max.chisto@gmail.com>
2016-05-04 16:12:33 -0700

    Added implementation of parallel fibonacci based on parMap

--- parallelProgramming/fibExample.hs
+++ parallelProgramming/fibExample.hs
@@ -1,7 +1,7 @@
 import Control.Parallel
 import Control.Monad
 import Control.Monad.Par
-import Control.Parallel.Strategies
+-- import Control.Parallel.Strategies
 import Text.Printf
  
 -- takes a function which in turn takes two args, takes two args, and executes the function
@@ -19,20 +19,25 @@ fib n = (+) l r
 
 
 -- implementation based on Par monad
-parFib :: Int -> Int
-parFib 0 = 0
-parFib 1 = 1
-parFib n = runPar $ do
+parFib1 :: Int -> Int
+parFib1 0 = 0
+parFib1 1 = 1
+parFib1 n = runPar $ do
     i <- new
     j <- new
-    fork (put i (fib n-1))
-    fork (put j (fib n-2))
+    fork (put i (fib $ n-1))
+    fork (put j (fib $ n-2))
     a <- get i
     b <- get j
     return (a+b)
 
 
+-- Implementation based on parMap / Par monad 
+parFib2 :: Int -> Int
+parFib2 0 = 0
+parFib2 1 = 1
+parFib2 n = sum $ runPar $ parMap fib [n-2,n-1]
 
  
 main = forM_ [2..45] $ \i ->
-            printf "n=%d => %d\n" i (parFib i)
+            printf "n=%d => %d\n" i (parFib1 i)
